8880aaea9a21748fee1a64d33a22f8caaebac735
CsuziCsongor <csuzicsongor@gmail.com>
2022-12-23 21:34:58 +0100

    2feladat

--- Battle Ship/Program.cs
+++ Battle Ship/Program.cs
@@ -1,29 +1,68 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using System.Diagnostics;
+
 namespace Battle_Ship
 {
-    internal static class Program
+    public partial class Form1 : Form
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
+        List<Button> playerPositionButtons;
+        List<Button> enemyPositionButtons;
+
+        Random rand = new Random();
+
+        int totalShips = 3;
+        int round = 10;
+        int playerScore;
+        int EnemyScore;
+
+     
+        public Form1()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
-            //A projekt elkezdodott, winter is coming
+            InitializeComponent();
+            RestartGame();
+        }
 
-            //Élesítem a tartalékainkat
+        private void EnemyPlayTimerEvent(object sender, EventArgs e)
+        {
 
-            //Készülődés
+        }
+        
+        private void AttackButtonEvent(object sender, EventArgs e)
+        {
 
-            //Kész
+        }
+
+        private void PlayerPositionButtonEvent(object sender, EventArgs e)
+        {
+
+        }
+
+        private void RestartGame()
+        {
+            playerPositionButtons = new List<Button> { w1, w2, w3, w4, x1, x2, x3, x4, y1, y2, y3, y4, z1, z2, z3, z4 };
+            enemyPositionButtons = new List<Button> { a1, a2, a3, a4, b1, b2, b3, b4, c1, c2, c3, c4, d1, d2, d3, d4 };
+
+            EnemyLocationListBox.Items.Clear();
+
+            EnemyLocationListBox.Text = null;
+
+            txtHelp.Text = "1. Click on three different locations from above to start!";
+
+
+        }
+
+        private void EnemyLocationPicker()
+        {
+
         }
     }
 }
